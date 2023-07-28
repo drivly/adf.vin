86faec44495c588d567d633f92d68f4f16dad4cb
Samuel Lippert <lippertz@gmail.com>
2023-07-28 15:43:50 -0500

    Valid file name

--- src/router.js
+++ src/router.js
@@ -49,7 +49,7 @@ router.get('/api', (request) => {
 </adf>`, {
 		headers: {
 			'Content-Type': 'application/x-adf+xml',
-			'Content-Disposition': `inline; filename="${createQuery}_${firstName || 'first'}_${lastName || 'last'}.adf"`
+			'Content-Disposition': `inline; filename="${createQuery.replace(':','_')}_${firstName || 'first'}_${lastName || 'last'}.adf"`
 		}
 	});
 });
