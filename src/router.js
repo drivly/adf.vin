af22a8dddaf5150bd58cf3031c420af7b44b2a58
Samuel Lippert <lippertz@gmail.com>
2023-07-30 13:22:45 -0500

    Add decode helper

--- src/router.js
+++ src/router.js
@@ -26,13 +26,12 @@ router.get('/api', (request) => {
 		created,
 	} = request.query;
 	const yearQuery = buildYearString(yearLower, yearUpper, year)
-	const createQuery = created && decodeURIComponent(created) || new Date().toISOString()
 	return new Response(`<?XML VERSION "1.0"?>
 <?ADF VERSION "1.0"?>
 <adf>
 	<prospect>
-		<requestdate>${createQuery}</requestdate>
-		<vehicle${interest ? ` interest="${interest}"` : ''}${newUsedStatus ? ` status="${newUsedStatus}"` : ''}>{${yearQuery ? (`
+		<requestdate>${decode(created) || new Date().toISOString()}</requestdate>
+		<vehicle${interest ? ` interest="${decodeURIComponent(interest)}"` : ''}${newUsedStatus ? ` status="${decodeURIComponent(newUsedStatus)}"` : ''}>{${yearQuery ? (`
 			<year>${yearQuery}</year>`) : ''}${make ? (`
 			<make>${decodeURIComponent(make)}</make>`) : ''}${model ? (`
 			<model>${decodeURIComponent(model)}</model>`) : ''}${trim ? (`
@@ -55,7 +54,7 @@ router.get('/api', (request) => {
 		</customer>
 		<vendor>
 			<contact>
-				<name part="full">${vendor && decodeURIComponent(vendor) || 'Cloud Motors'}</name>
+				<name part="full">${decode(vendor) || 'Cloud Motors'}</name>
 			</contact>
 		</vendor>
 	</prospect>
@@ -67,24 +66,17 @@ router.get('/api', (request) => {
 	});
 });
 
-// POST to the collection (we'll use async here)
-router.post('/api', async (request) => {
-	const content = await request.text();
-
-	return new Response(content, {
-		headers: {
-			'Content-Type': 'application/x-adf+xml',
-		}
-	});
-});
-
 // 404 for everything else
 router.all('*', () => new Response('Not Found.', { status: 404 }));
 
 function buildYearString(yearLower, yearUpper, year) {
-	return !yearLower && !yearUpper ? decodeURIComponent(year) :
+	return !yearLower && !yearUpper ? decode(year) :
 		yearLower === yearUpper ? decodeURIComponent(yearLower) :
-			`${decodeURIComponent(yearLower)}-${decodeURIComponent(yearUpper)}`
+			`${decode(yearLower)}-${decode(yearUpper)}`
+}
+
+function decode(s) {
+	return s && decodeURIComponent(s) || undefined
 }
 
 export default router;
